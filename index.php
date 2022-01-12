700c3785ce3362b8e33dd6546cd05ac4bba6fc87
kokihara <nov17hk@gmail.com>
2022-01-12 22:10:54 +0900

    html記述

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
- echo 'Hello World';
-?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>画像アップロード掲示板</title>
+  <style>
+  body {
+    text-align: center;
+    font-family: Arial, sans-serif;
+  }
+  </style>
+</head>
+<body>
+</body>
+</html>
