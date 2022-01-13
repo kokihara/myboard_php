92d210623d9a54358733b8ebd0acdc78620036c2
kokihara <nov17hk@gmail.com>
2022-01-13 20:52:06 +0900

    PHP部分修正

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<?php
+define('MAX_FILE_SIZE', 1 * 1024 * 1024);
+define('THUMBNAIL_WIDTH', 400);
+define('IMAGES_DIR', __DIR__ . '/images');
+define('THUMBNAIL_DIR', __DIR__ . '/thumbs');
+ini_set('display_errors', 1);
+if (!function_exists('imagecreatetruecolor')) {
+  echo 'Go not installed';
+  exit;
+
+function h($s) {
+  return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
+}
+require 'ImageUploader.php';
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -11,5 +28,10 @@
   </style>
 </head>
 <body>
+   <form action="" method="post" enctype="multipart/form-data">
+  <input type="hidden" name="MAX_FILE_SIZE" value="">
+  <input type="file" name="image">
+  <input type="submit" value="upload">
+</form>
 </body>
 </html>
